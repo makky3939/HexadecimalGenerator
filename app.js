2790c2fa9285894be22e5babf999dc03d1da028a
Makky <makky.4d6b.3f5@gmail.com>
2013-07-29 13:30:09 +0900

    表示する進数を変更出来るようにしました
    1, 2, 3 キーを押すと切り替わります

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 var app = (function(){
 
-function _init() {
-
+var gg = 16;
 document.onkeydown = function (e){
 
-if (!e)	e = window.event;
-var str = "";
 
-for(var i=0;i<10000;i++){
-var hage = Math.floor(Math.random()*255+1).toString(16);
-str = str+" "+hage;
-}
-console.log(hage.toString(16));
+	if (!e)	e = window.event;
+
+	if(event.keyCode == 49){
+		gg = 16;
+	}else if(event.keyCode == 50){
+		gg = 10;
+	}else if(event.keyCode == 51){
+		gg = 2;
+	}
+	
+		var str = "";
+
+	for(var i=0;i<10000;i++){
+		var hage = Math.floor(Math.random()*255+1).toString(gg);
+		str = str+" "+hage;
+	}
+
+	//console.log(hage.toString(16));
 
 $("body").html(str);
+//console.log(event.keyCode);
 
 };
-}
-
-_init();
   
 return 0;
 
